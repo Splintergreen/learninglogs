09522e5c2dcb5ff8dd7de43816e05e29288ffeae
ssuhotskiy <ssuhotskiy@gmail.com>
2023-03-11 15:45:03 +0300

    add deploy script; add flake8 config file

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# exit if errors
+set -e
+
+echo "Deployment started ..."
+
+cd coding/learninglogs/
+
+source venv/bin/activate
+echo "Virtual Env Activated !"
+
+git pull origin master
+echo "New changes copied to server !"
+
+echo "Installing Dependencies..."
+pip install -r requirements.txt --no-input
+
+echo "Serving Static Files..."
+# cd /learninglogs and remember previous directory
+pushd learninglogs
+python manage.py collectstatic --noinput
+
+echo "Running Database migration"
+python manage.py makemigrations
+python manage.py migrate
+# return to previous directory
+popd
+
+deactivate
+echo "Virtual Env Deactivated !"
+
+# kill and restart gunucorn process
+systemctl status gunicorn |  sed -n 's/.*Main PID: \(.*\)$/\1/g p' | cut -f1 -d' ' | xargs kill -HUP
+
+echo "Deployment Finished!"
